6b3b472197620f91157c968367f2599e8ec45652
trudeaujt <trudeaujt@users.noreply.github.com>
2022-05-02 01:47:48 +0000

    deploy: 0e8e93e727f0dd26379cb6f57fdb5156d9aeab95

--- src/components/Content.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Card from "./Card";
-import data from "./../data";
-
-export default function Content() {
-  const cards = data.map((card) => <Card key={card.id} {...card} />);
-  return <div className="content">{cards}</div>;
-}
